01e348ec8807f0150faa399fdbadb9852f5613cc
miqueas <micahmccallum@gmail.com>
2021-01-03 17:36:41 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+class App < Sinatra::Base  
+  configure :development do
+    set :database, {adapter: "sqlite3", database: "db/database.db"}
+  end
+
 end
